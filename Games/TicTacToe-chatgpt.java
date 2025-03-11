e65580c4d65f8d7a400ca6fe5842042494ad403c
Raja Kumar <teamaayushgupta@gmail.com>
2025-03-11 20:39:50 +0530

    initial commit

---
+++ Games/TicTacToe-chatgpt.java
@@ -0,0 +1,109 @@
+import java.util.Scanner;
+
+public class TicTacToeGPT {
+    private static char[][] board = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    private static char currentPlayer = 'X';
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        printBoard();
+
+        while (true) {
+            int[] move = getPlayerMove(scanner);
+
+            if (isValidMove(move)) {
+                makeMove(move);
+                printBoard();
+
+                if (isWinner()) {
+                    System.out.println("Player " + currentPlayer + " wins!");
+                    break;
+                } else if (isBoardFull()) {
+                    System.out.println("It's a draw!");
+                    break;
+                }
+
+                switchPlayer();
+            } else {
+                System.out.println("Invalid move. Try again.");
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static void printBoard() {
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
+    private static int[] getPlayerMove(Scanner scanner) {
+        System.out.print("Player " + currentPlayer + ", enter your move (row and column): ");
+        int row = scanner.nextInt();
+        int col = scanner.nextInt();
+        return new int[]{row, col};
+    }
+
+    private static boolean isValidMove(int[] move) {
+        int row = move[0];
+        int col = move[1];
+
+        return row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ';
+    }
+
+    private static void makeMove(int[] move) {
+        int row = move[0];
+        int col = move[1];
+        board[row][col] = currentPlayer;
+    }
+
+    private static void switchPlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    private static boolean isWinner() {
+        return checkRows() || checkColumns() || checkDiagonals();
+    }
+
+    private static boolean checkRows() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkColumns() {
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] != ' ' && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkDiagonals() {
+        return (board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
+               (board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0]);
+    }
+
+    private static boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
